1be54bd223d50856ed3e142e469c204ecf7b1508
constantinsuiu <constantin.suiu@me.com>
2019-01-10 22:12:25 +0100

    Updated tests to work on both Windows and Mac

--- utilities/driver.py
+++ utilities/driver.py
@@ -8,12 +8,19 @@ from utilities.utilities import Utilities
 class DriverProvider:
     driver_provider = None
 
+
+
     def __init__(self, browser='chrome'):
+        if sys.platform == "win32":
+            chrome = '\chromedriver.exe'
+        else:
+            chrome = '/chromedriver'
         utilities = Utilities()
         # path = utilities.move_up_directory(os.getcwd(), 1)
         currentFilePath = os.path.realpath(__file__)
         new_path = utilities.move_up_directory(currentFilePath, 1)
+        print(new_path + chrome)
         if browser.lower() == 'chrome':
-            self.driver_provider = webdriver.Chrome(new_path + '/chromedriver')
+            self.driver_provider = webdriver.Chrome(new_path + chrome)
         elif browser.lower() == 'firefox':
             self.driver_provider = webdriver.Firefox()
